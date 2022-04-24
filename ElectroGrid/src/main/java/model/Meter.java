dfa3c07c4c264f7f548f7856b7084e8848499f2d
IndrachapaSenanayaka <senanayakeindrachapa@gmail.com>
2022-04-25 05:02:21 +0530

    meter registration was created

--- ElectroGrid/src/main/java/model/Meter.java
+++ ElectroGrid/src/main/java/model/Meter.java
@@ -1,5 +1,50 @@
 package model;
 
+import java.sql.*;
+
 public class Meter {
+	
+Connection con = null;
+	
+	//meter registration 
+	public String insertMeter(String meterCode, String premisesID, String electricityAccountNo, String manufactureDate)
+	{
+		String output = "";
+		
+		try
+		{
+			con = DBconfig.getConnection();
+			
+			
+			if (con == null)
+			{return "Error while connecting to the database for inserting."; }
+			
+			// create a prepared statement
+			String query = " insert into Meter(`meterID`,`meterCode`,`premisesID`,`electricityAccountNo`,`manufactureDate`)"
+							+ " values (?, ?, ?, ?, ?)";
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, meterCode);
+			preparedStmt.setString(3, premisesID);
+			preparedStmt.setInt(4, Integer.parseInt(electricityAccountNo));
+			preparedStmt.setString(5, manufactureDate);
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Inserted successfully";
+		}
+		catch (Exception e)
+		{
+			output = "Error while inserting the meter.";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
 
 }
