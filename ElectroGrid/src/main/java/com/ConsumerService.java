41e81806e360115ce6e9a8fc2ccbc60aec67df8b
IT20096748 <it20096748@my.sliit.lk>
2022-04-25 14:21:12 +0530

    get request consumer service created

--- ElectroGrid/src/main/java/com/ConsumerService.java
+++ ElectroGrid/src/main/java/com/ConsumerService.java
@@ -1,5 +1,35 @@
 package com;
+import model.Consumer;
+import javax.ws.rs.Consumes;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
+@Path("/Consumers")
 
 public class ConsumerService {
 
+	Consumer consumerObj = new Consumer();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readConsumers()
+	{
+
+		return consumerObj.readConsumers();
+	}
 }
