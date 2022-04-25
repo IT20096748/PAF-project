77d6966a51607aeab60c7c4c240137214a0880f1
IT20096748 <it20096748@my.sliit.lk>
2022-04-25 13:58:33 +0530

    insert consumers created

--- ElectroGrid/src/main/java/model/Consumer.java
+++ ElectroGrid/src/main/java/model/Consumer.java
@@ -23,4 +23,54 @@ public class Consumer {
 		
 		return con;
 	}
+public String insertConsumer(String accountNo , String firstName, String lastName, String gender,String occupation, String phoneNumber, String email, String password, String province , String city , String street, String postalCode )
+{
+	String output = "";
+	
+	try
+	{
+		Connection con = connect();
+		
+		if (con == null)
+		{return "Error while connecting to the database for inserting."; }
+		 
+
+		// create a prepared statement
+		String query = " insert into consumer(`consumerNo`,`accountNo`,`firstName`,`lastName`,`gender`,`occupation`,`phoneNumber`,`email`,`password`,`province`,`city`,`street`,`postalCode`)"
+						+ " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		PreparedStatement preparedStmt = con.prepareStatement(query);
+		
+		// binding values
+		preparedStmt.setInt(1, 0);
+		preparedStmt.setInt(2,Integer.parseInt(accountNo));
+		preparedStmt.setString(3, firstName);
+		preparedStmt.setString(4, lastName);
+		preparedStmt.setString(5, gender);
+		preparedStmt.setString(6, occupation);
+		preparedStmt.setString(7, phoneNumber);
+		preparedStmt.setString(8, email);
+		preparedStmt.setString(9, password);
+		preparedStmt.setString(10,province);
+		preparedStmt.setString(11,city);
+		preparedStmt.setString(12, street);
+		preparedStmt.setInt(13, Integer.parseInt(postalCode));
+		
+		
+		// execute the statement
+		preparedStmt.execute();
+		con.close();
+		
+		output = "Inserted successfully";
+	}
+	catch (Exception e)
+	{
+		output = "Error while inserting the Consumer.";
+		System.err.println(e.getMessage());
+	}
+	
+	return output;
+	
+	
+}
 }
