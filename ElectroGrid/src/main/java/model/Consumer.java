fb567044f8dbfbbb427ce5981d55f0c8e54acf2d
IT20096748 <it20096748@my.sliit.lk>
2022-04-25 14:15:55 +0530

    delete consumer function created

--- ElectroGrid/src/main/java/model/Consumer.java
+++ ElectroGrid/src/main/java/model/Consumer.java
@@ -210,5 +210,38 @@ public String updateConsumer(String consumerNo,String accountNo ,String firstNam
 	
 	return output;
 }
+public String deleteConsumer(String accountNo)
+{
+	String output = "";
+	
+	try
+	{
+		Connection con = connect();
+		
+		if (con == null)
+		{return "Error while connecting to the database for deleting."; }
+		
+		// create a prepared statement
+		String query = "delete from consumer where accountNo=?";
+		
+		PreparedStatement preparedStmt = con.prepareStatement(query);
+		
+		// binding values
+		preparedStmt.setInt(1, Integer.parseInt(accountNo));
+		
+		// execute the statement
+		preparedStmt.execute();
+		con.close();
+		
+		output = "Deleted successfully";
+	}
+	catch (Exception e)
+	{
+		output = "Error while deleting the Consumer.";
+		System.err.println(e.getMessage());
+	}
+	
+	return output;
+}
 
 }
