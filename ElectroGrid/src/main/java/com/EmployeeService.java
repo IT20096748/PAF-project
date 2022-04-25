51b0038c8f2853ab82166f8db0d64ca8f847ba85
IT20096748 <it20096748@my.sliit.lk>
2022-04-25 14:53:18 +0530

    POST request for employee service created

--- ElectroGrid/src/main/java/com/EmployeeService.java
+++ ElectroGrid/src/main/java/com/EmployeeService.java
@@ -31,4 +31,24 @@ public class EmployeeService {
 
 		return employeeObj.readEmployees();
 	}
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertEmployee(@FormParam("empID") String empID,
+							 @FormParam("firstName") String firstName,
+							 @FormParam("lastName") String lastName,
+							 @FormParam("gender") String gender,
+							 @FormParam("jobTitle") String jobTitle,
+							 @FormParam("phoneNumber") String phoneNumber,
+							 @FormParam("email") String email,
+							 @FormParam("password") String password,
+							 @FormParam("province") String province,
+							 @FormParam("city") String city ,
+							 @FormParam("street") String street,
+							 @FormParam("postalCode") String postalCode)
+	{
+		String output = employeeObj.insertEmployee(empID,firstName,lastName,gender,jobTitle,phoneNumber,email,password,province,city,street,postalCode);
+		return output;
+	}
 }
