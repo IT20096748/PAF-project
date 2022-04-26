079127ca3e442ad4aab5133e8dc5d052f2104436
IT20096748 <it20096748@my.sliit.lk>
2022-04-26 13:08:00 +0530

    GET request for complaint service created

--- ElectroGrid/src/main/java/com/ComplaintService.java
+++ ElectroGrid/src/main/java/com/ComplaintService.java
@@ -3,8 +3,35 @@
  */
 package com;
 
+import model.Complaint;
+
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/Complaints")
+
 
 public class ComplaintService {
+Complaint cObj = new Complaint();
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems()
+	{
+
+		return cObj.readComplaints();
+	}
 	
 
 	
