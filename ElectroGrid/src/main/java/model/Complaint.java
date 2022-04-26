17dbf59961c364e69f3f5ac31002ad2bf89ec198
IT20096748 <it20096748@my.sliit.lk>
2022-04-26 13:02:38 +0530

    retrieve operation for complaint handling created

--- ElectroGrid/src/main/java/model/Complaint.java
+++ ElectroGrid/src/main/java/model/Complaint.java
@@ -73,4 +73,63 @@ public class Complaint {
 		
 		return output;
 	}
+	public String readComplaints()
+	{
+		String output = "";
+		
+		try
+		{
+				Connection con = connect();
+				
+				if (con == null)
+				{return "Error while connecting to the database for reading."; }
+	
+				// Prepare the html table to be displayed
+				output ="<html><head><title>Consumer</title>"
+						+ "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">"
+						+"</head><body><table class=\"table\" border='1'><tr>"
+						+"<table border='1'><tr><th>Complaint ID&nbsp;</th>"
+						+"<th>Complaint Type&nbsp;</th>" 
+						+"<th>Description&nbsp;</th>" 
+						+"<th>Update</th><th>Remove</th></tr>";
+				
+				String query = "select * from complaint";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+				
+				// iterate through the rows in the result set
+				while (rs.next())
+				{
+					String cID = rs.getString("cID");
+					String type = rs.getString("type");
+					String cdesc = rs.getString("cdesc");
+					
+					
+					// Add into the html table
+					output += "<tr><td>" + cID + "</td>";
+					output += "<td>" + type + "</td>";
+					output += "<td>" + cdesc + "</td>";
+					
+					// buttons
+					output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-success'></td>" 
+							+ "<td><form method='post' action='items.jsp'>" 
+							+ "<input name='btnRemove' type='button' value='Remove' class='btn btn-danger'>"
+							+ "<input name='cID' type='hidden' value='" + cID
+							+ "'>" + "</form></td></tr>";
+				}
+				
+				con.close();
+				
+				// Complete the html table
+				output += "</table>";
+		}
+		catch (Exception e)
+		{
+			output = "Error while reading the complaints.";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
 }
