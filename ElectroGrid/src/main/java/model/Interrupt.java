4ee8fbad38922925af31c7e1288b5ec9c0627179
Weesinghe.W.M.P.D <it20019204@my.sliit.lk>
2022-04-26 20:24:03 +0530

    interrupt detail added

--- ElectroGrid/src/main/java/model/Interrupt.java
+++ ElectroGrid/src/main/java/model/Interrupt.java
@@ -10,7 +10,7 @@ private Connection connect()
  Class.forName("com.mysql.jdbc.Driver");
 
  //Provide the correct details: DBServer/DBName, username, password
- con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/egsystem", "root", "");
+ con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/electro_grid", "root", "");
  }
  catch (Exception e)
  {e.printStackTrace();}
