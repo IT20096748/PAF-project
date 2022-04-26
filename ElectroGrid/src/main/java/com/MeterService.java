45fd2fcc208d3b53a8c64fea0c64e560acc34ceb
IndrachapaSenanayaka <senanayakeindrachapa@gmail.com>
2022-04-26 19:02:12 +0530

    get meter details as JSON format created

--- ElectroGrid/src/main/java/com/MeterService.java
+++ ElectroGrid/src/main/java/com/MeterService.java
@@ -20,31 +20,42 @@ import com.google.gson.JsonParser;
 
 import model.Meter;
 
+
+
 @Path("/Meters")
 public class MeterService {
 	
-Meter meterObj = new Meter();
+	Meter meterObj = new Meter();
 	
 	@GET
 	@Path("/View-All")
 	@Produces(MediaType.TEXT_HTML)
-	public String readMeters()
+	public String readItems()
 	{
 
 		return meterObj.readMeters();
 	}
 	
+
+	@GET
+	@Path("/Get/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String viewMeter(@PathParam("id") String meterID)
+	{
+
+		return meterObj.getMeter(meterID);
+	}
 	
 	@POST
 	@Path("/Create")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String insertMeter(@FormParam("meterCode") String meterCode,
-							 @FormParam("premisesID") String premisesID,
-							 @FormParam("electricityAccountNo") String electricityAccountNo,
-							 @FormParam("manufactureDate") String manufactureDate)
+	public String insertItem(@FormParam("itemCode") String itemCode,
+							 @FormParam("itemName") String itemName,
+							 @FormParam("itemPrice") String itemPrice,
+							 @FormParam("itemDesc") String itemDesc)
 	{
-		String output = meterObj.insertMeter(meterCode, premisesID, electricityAccountNo, manufactureDate);
+		String output = meterObj.insertMeter(itemCode, itemName, itemPrice, itemDesc);
 		return output;
 	}
 	
@@ -52,33 +63,33 @@ Meter meterObj = new Meter();
 	@Path("/Modify/{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String updateMeter(String meterData, @PathParam("id") String meterID)
+	public String updateItem(String itemData, @PathParam("id") String itemID)
 	{
 		//Convert the input string to a JSON object
-		JsonObject meterObject = new JsonParser().parse(meterData).getAsJsonObject();
+		JsonObject itemObject = new JsonParser().parse(itemData).getAsJsonObject();
 		
 		//Read the values from the JSON object
-		String meterCode = meterObject.get("meterCode").getAsString();
-		String premisesID = meterObject.get("premisesID").getAsString();
-		String electricityAccountNo = meterObject.get("electricityAccountNo").getAsString();
-		String manufactureDate = meterObject.get("manufactureDate").getAsString();
+		String itemCode = itemObject.get("itemCode").getAsString();
+		String itemName = itemObject.get("itemName").getAsString();
+		String itemPrice = itemObject.get("itemPrice").getAsString();
+		String itemDesc = itemObject.get("itemDesc").getAsString();
 		
-		String output = meterObj.updateMeter(meterID, meterCode, premisesID, electricityAccountNo, manufactureDate);
+		String output = meterObj.updateMeter(itemID, itemCode, itemName, itemPrice, itemDesc);
 		
 		return output;
 	}
-		
+	
 	
 	@DELETE
 	@Path("/Delete")
 	@Consumes(MediaType.APPLICATION_XML)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String deleteMeter(String meterData)
+	public String deleteItem(String meterData)
 	{
 		//Convert the input string to an XML document
 		Document doc = Jsoup.parse(meterData, "", Parser.xmlParser());
 		
-		//Read the value from the element <meterID>
+		//Read the value from the element <itemID>
 		String meterID = doc.select("meterID").text();
 		
 		String output = meterObj.deleteMeter(meterID);
