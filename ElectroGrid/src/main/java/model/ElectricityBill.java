dc7f14993ce1d2a79c64a445fa8920b82f7ef430
IndrachapaSenanayaka <senanayakeindrachapa@gmail.com>
2022-04-26 19:08:17 +0530

    View bill list created

--- ElectroGrid/src/main/java/model/ElectricityBill.java
+++ ElectroGrid/src/main/java/model/ElectricityBill.java
@@ -75,4 +75,70 @@ public class ElectricityBill {
 		return output;
 	}
 	
+	//View bill list
+	public String viewBills()
+	{
+		String output = "";
+		
+		try
+		{
+				con = DBconfig.getConnection();
+				
+				if (con == null)
+				{return "Error while connecting to the database for reading."; }
+	
+				// Prepare the html table to be displayed
+				output = "<table border='1'><tr><th>Bill ID</th><th>Electricity Account No</th>" +
+						"<th>Month</th>" +
+						"<th>Units Consumed</th>" +
+						"<th>Total Cost Of Supply Rs/=</th>" + 
+						"<th>Status</th>" +
+						"<th>Update</th><th>Remove</th></tr>";
+				
+				String query = "select * from Bill";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+				
+				// iterate through the rows in the result set
+				while (rs.next())
+				{
+					String billID = rs.getString("billID");
+					String electricityAccountNo = rs.getString("electricityAccountNo");
+					String month = rs.getString("month");
+					String unitsConsumed = Integer.toString(rs.getInt("unitsConsumed"));
+					String totalCostOfSupply = Double.toString(rs.getDouble("totalCostOfSupply"));
+					String status = rs.getString("status");
+					
+					// Add into the html table
+					output += "<tr><td>" + billID + "</td>";
+					output += "<td>" + electricityAccountNo + "</td>";
+					output += "<td>" + month + "</td>";
+					output += "<td>" + unitsConsumed + "</td>";
+					output += "<td>" + totalCostOfSupply + "</td>";
+					output += "<td>" + status + "</td>";
+	
+					// buttons
+					output += "<td><form method='post' action='items.jsp'>" 
+							+ "<input name='btnUpdate' type='submit' value='Update' class='btn btn-secondary'>"
+							+ "<input name='itemID' type='hidden' value='" + billID
+							+ "'>" + "</form></td>"
+							+ "<td><form method='post' action='items.jsp'>" 
+							+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+							+ "<input name='itemID' type='hidden' value='" + billID
+							+ "'>" + "</form></td></tr>";
+				}
+				
+				con.close();
+				
+				// Complete the html table
+				output += "</table>";
+		}
+		catch (Exception e)
+		{
+			output = "Error while reading the bills.";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
 }
