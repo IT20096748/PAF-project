70cf28e0e1cd3a7668f3d3339b1ab134778630fe
IndrachapaSenanayaka <senanayakeindrachapa@gmail.com>
2022-04-26 19:23:44 +0530

    date and time detector was created

--- ElectroGrid/src/main/java/model/CommonDButil.java
+++ ElectroGrid/src/main/java/model/CommonDButil.java
@@ -19,5 +19,27 @@ public class CommonDButil {
 		
 		return ID;
 	}
+	
+	//date time
+	public static String DateTime(){
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+		Date date = new Date();
+		String dateTime = sdf.format(date);
+		
+		String ID = dateTime;
+		
+		return ID;
+	}
+	
+	//month
+	public static String Month(){
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("MMM-yyyy");
+		Date date = new Date();
+		String month = sdf.format(date);
+		
+		return month;
+	}
 
 }
